3e1270ca0d24341aa3bb8a3ec25958cc850391c5
Kai Zhi Chiang <kchiang@u82z01s05.42kl.edu.my>
2025-03-16 18:24:14 +0800

    updated c05

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -1,41 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/15 14:59:11 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/16 11:44:30 by kchiang          ###   ########.fr       */
+/*   Created: 2025/03/16 11:46:28 by kchiang           #+#    #+#             */
+/*   Updated: 2025/03/16 12:49:23 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 //#include <limits.h>
 
-int	ft_sqrt(int nb)
+int	ft_is_prime(int nb)
 {
-	long	sqrt;
+	long	div;
 
-	sqrt = 0;
-	while (sqrt * sqrt <= nb)
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0 || nb < 2)
+		return (0);
+	div = 3;
+	while (div * div <= nb)
 	{
-		if (sqrt * sqrt == nb)
-			return ((int)sqrt);
-		sqrt++;
+		if (nb % div == 0)
+			return (0);
+		div += 2;
 	}
-	return (0);
+	return (1);
 }
 
 /*
 int	main(void)
 {
-	int	nb = INT_MAX;
-	int	sqrt = ft_sqrt(nb);
-	if (sqrt)
-		printf("sq_root of %d = %d\n", nb, sqrt);
+	int	i = INT_MAX;
+
+	if (ft_is_prime(i))
+		printf("%d is prime\n", i);
 	else
-		printf("%d is irrational\n", nb);
+		printf("%d is not prime\n", i);
 	return (0);
 }
 */
