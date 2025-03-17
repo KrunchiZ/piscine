31c1f6d74d29b00ef84be7de6ef7fa7621da2d70
Kai Zhi Chiang <kchiang@u81z01s06.42kl.edu.my>
2025-03-17 10:50:04 +0800

    updated c00 putnbr

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -5,42 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/02 14:34:29 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/03 13:13:07 by kchiang          ###   ########.fr       */
+/*   Created: 2025/03/11 16:20:48 by kchiang           #+#    #+#             */
+/*   Updated: 2025/03/11 16:59:30 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <limits.h>
 
-void	print_digit(int nb)
+//print last digit with %, remove it with /, then recall until 0
+void	print_nbr(int nb)
 {
-	char	digit;
-
-	if (nb == 0)
+	if (!nb)
 		return ;
-	digit = (nb % 10) + '0';
-	nb /= 10;
-	print_digit(nb);
-	write(1, &digit, 1);
+	print_nbr(nb / 10);
+	nb = nb % 10 + '0';
+	write(1, &nb, 1);
+	return ;
 }
 
+//cover case 0 and INT_MIN, convert negative to positive
 void	ft_putnbr(int nb)
 {
-	if (nb == 0)
+	if (!nb)
 		write(1, "0", 1);
+	else if (nb == INT_MIN)
+		write(1, "-2147483648", 11);
 	else if (nb < 0)
 	{
 		nb *= -1;
 		write(1, "-", 1);
-		print_digit(nb);
+		print_nbr(nb);
 	}
 	else
-		print_digit(nb);
+		print_nbr(nb);
+	return ;
 }
 
-
+/*
 int	main(void)
 {
-	ft_putnbr(-2147483648);
+	ft_putnbr(INT_MAX);
+	return (0);
 }
-
+*/
