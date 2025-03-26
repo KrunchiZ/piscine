f58cae3ae11a89e2f159de548dacc86c9cb29eda
kaizhi <kaizhi3006@gmail.com>
2025-03-26 19:53:36 +0800

    edited header

--- c08/ex05/ft_stock_str.h
+++ c08/ex05/ft_stock_str.h
@@ -6,16 +6,12 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:23:28 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/26 19:13:19 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/03/26 19:53:04 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STOCK_STR_H
 # define FT_STOCK_STR_H
-
-void	ft_show_tab(t_stock_str *par);
-t_stock_str	*ft_strs_to_tab(int ac, char **av);
-void	free_host(t_stock_str *host, int depth);
 
 typedef struct	s_stock_str
 {
@@ -23,5 +19,9 @@ typedef struct	s_stock_str
 	char	*str;
 	char	*copy;
 } t_stock_str;
+
+void	ft_show_tab(t_stock_str *par);
+t_stock_str	*ft_strs_to_tab(int ac, char **av);
+void	free_host(t_stock_str *host, int depth);
 
 #endif
