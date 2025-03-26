bf02fab374bfb3334fa8998d47009394ea76c22c
kaizhi <kaizhi3006@gmail.com>
2025-03-26 19:50:11 +0800

    created main for test

---
+++ c08/ex05/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 19:42:23 by kchiang           #+#    #+#             */
+/*   Updated: 2025/03/26 19:48:48 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	t_stock_str	*host;
+
+	if (argc < 2)
+		return (0);
+	host = ft_strs_to_tab(argc - 1, &argv[1]);
+	if (host)
+	{
+		ft_show_tab(host);
+		free_host(host, argc);
+	}
+	return (0);
+}
