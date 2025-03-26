1af6fcd5b5c765086f596007c4e4b7821255f6e9
kaizhi <kaizhi3006@gmail.com>
2025-03-26 18:05:44 +0800

    finished ft_strs_to_tab.c

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -6,13 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:21:57 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/26 17:51:21 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/03/26 18:05:25 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ft_stock_str.h"
 
+//strlen the length of argv and store it in struct size
 int	ft_strlen(char *str)
 {
 	int	len;
@@ -23,13 +24,15 @@ int	ft_strlen(char *str)
 	return (len);
 }
 
+//strdup malloc with struct size
 char	*strdup(char *src, int length)
 {
-	//malloc with length + 1
 	char	*copy;
 	int		i;
 
 	copy = (char *)malloc(sizeof(char) * (length + 1));
+	if (copy == NULL)
+		return (NULL);
 	i = 0;
 	while (src[i])
 	{
@@ -40,20 +43,38 @@ char	*strdup(char *src, int length)
 	return (copy);	
 }
 
+//free previous malloc copy in host when there is error
+void	free_host(t_stock_str *host, int depth)
+{
+	int	i;
+
+	i = 0;
+	while (i < depth)
+		free(host[i++].copy);
+	return ;
+}
+
+//parse in argv[1] as av, ac as max size for host struct malloc
 t_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
 	int			i;
 	t_stock_str	*host;
 
 	host = (struct *)malloc(sizeof(t_stock_str) * ac);
+	if (host == NULL)
+		return (NULL);
 	host[ac]->str = 0;
 	i = 0;
 	while (i < ac)
 	{
-		//loop thru argv to get all the strings
-		host[i]->size = ft_strlen(av[i]);
-		host[i]->str = av[i];
-		host[i]->copy = strdup(av[i], size);
+		host[i].size = ft_strlen(av[i]);
+		host[i].str = av[i];
+		host[i].copy = strdup(av[i], host[i].size);
+		if (host[i]->copy == NULL)
+		{
+			free_host(host, i);
+			return (NULL);
+		}
 		i++;
 	}
 	return (host);
