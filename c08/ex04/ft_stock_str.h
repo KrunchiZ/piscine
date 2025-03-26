a1f4f4e0ec1b4287282004b9b0df2ae387d2ba9c
kaizhi <kaizhi3006@gmail.com>
2025-03-26 16:25:46 +0800

    added ex04

---
+++ c08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 16:23:28 by kchiang           #+#    #+#             */
+/*   Updated: 2025/03/26 16:25:26 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct	s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+} t_stock_str;
+
+#endif
