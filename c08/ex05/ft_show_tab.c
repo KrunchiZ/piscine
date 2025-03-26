5cc82b43b8451ac3ffc4c7be5cdea099ef6d8b68
kaizhi <kaizhi3006@gmail.com>
2025-03-26 19:24:20 +0800

    ex05 wip, updated prototype in stock_str.h

--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 18:45:03 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/26 18:56:22 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/03/26 19:23:40 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,32 @@
 
 void	ft_putstr(char *str)
 {
+	while (*str)
+		write(1, str++, 1);
+	write(1, "\n", 1);
+	return ;
+}
+
+void	print_nbr(int nbr)
+{
+	if (nbr == 0)
+		return ;
+	print_nbr(nbr / 10);
+	nbr = nbr % 10 + '0';
+	write(1, &nbr, 1);
+	return ;
 }
 
 void	ft_putnbr(int nb)
 {
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
+	}
+	print_nbr(nb);
+	write(1, "\n", 1);
+	return ;
 }
 
 void	ft_show_tab(t_stock_str *par)
--- c08/ex05/ft_stock_str.h
+++ c08/ex05/ft_stock_str.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:23:28 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/26 18:53:51 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/03/26 19:13:19 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	ft_show_tab(t_stock_str *par);
 t_stock_str	*ft_strs_to_tab(int ac, char **av);
+void	free_host(t_stock_str *host, int depth);
 
 typedef struct	s_stock_str
 {
