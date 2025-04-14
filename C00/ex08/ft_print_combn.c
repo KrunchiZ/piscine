64cb2fed6652064d38679355f71010d5fa053624
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-04-14 12:45:00 +0800

    ft_print_combn wip, managed to make it work if n = 2, have to make it scalable next

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 17:46:34 by kchiang           #+#    #+#             */
-/*   Updated: 2025/03/02 19:32:56 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/04/14 12:43:41 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,44 @@
 
 void	ft_print_combn(int n)
 {
+	int		comb[9];
+	int		i;
+	int		j;
+	int		depth;
 	char	digit;
 
-	if (n == 0)
-		return ;
-	digit = (--n) + '0';
-	ft_print_combn(n);
-	write(1, &digit, 1);
+	i = 0;
+	depth = 0;
+	while (i < 10)
+	{
+		comb[depth] = i;
+		depth++;
+		if (depth == n)
+		{
+			j = 0;
+			while (j < n)
+			{
+				digit = comb[j] + '0';
+				write(1, &digit, 1);
+				j++;
+			}
+			if (comb[0] != (10 - n))
+				write(1, ", ", 2);
+			depth--;
+		}
+		if (i == 9)
+		{
+			depth--;
+			i = comb[depth] + 1;
+		}
+		else
+			i++;
+	}
+	return ;
 }
 
 int	main(void)
 {
-	ft_print_combn(2);
+	ft_print_combn(3);
+	return (0);
 }
