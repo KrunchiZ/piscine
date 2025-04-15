c1993e331fea6920b6736a99a0bac353a62854c6
kz_home_wsl <kaizhi3006@gmail.com>
2025-04-16 01:59:16 +0800

    fix solver prototype in header

--- rush01/ex00/header.h
+++ rush01/ex00/header.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:57:49 by kchiang           #+#    #+#             */
-/*   Updated: 2025/04/16 01:21:29 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/04/16 01:58:51 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	arg_is_invalid(int argc, char *argv);
 int	ft_strlen(char *str);
-int	solve_puzzle(int clue[4][4], int *answer);
+int	solve_puzzle(int clue[4][4], int *answer, int n, int pos);
 int	row_is_bad(int *answer, int clue[4][4], int pos);
 int	col_is_bad(int *answer, int clue[4][4], int pos);
 
