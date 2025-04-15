528184414ad1e5f68d8c659d1501b5562623f02b
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-04-15 15:26:48 +0800

    main.c wip, finished error check and parse clue and answer array

--- rush01/ex00/main.c
+++ rush01/ex00/main.c
@@ -6,11 +6,12 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 10:59:53 by kchiang           #+#    #+#             */
-/*   Updated: 2025/04/15 14:28:52 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/04/15 15:25:50 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
+#include <unistd.h>
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
@@ -28,25 +29,46 @@ int	arg_is_invalid(int argc, char *argv)
 
 	if (argc != 2 || ft_strlen(argv) != 31)
 		return (1);
-	space_index = 1;
-	while (space_index < 32)
+	space_index = 0;
+	while (space_index < 31)
 	{
-		if (argv[space_index] != ' ')
+		if (space_index % 2 && argv[space_index] != ' ')
 			return (1);
-		space_index += 2;
+		space_index++;
 	}
 	return (0);
 }
 
+void	parse_grid(int *clue, int *answer, char *argv)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	while (i < 16)
+		answer[i++] = 0;
+	i = 0;
+	j = 0;
+	while (i < 31)
+	{
+		if (i % 2 == 0)
+			clue[j++] = argv[i];
+		i++;
+	}
+	return ;
+}
+
 int	main(int argc, char *argv[])
 {
-	int	grid[16];
+	int	clue[16];
+	int	answer[16];
 
 	if (arg_is_invalid(argc, argv[1]))
 		write(1, "Error\n", 6);
 	else
 	{
-		write(1, "start_program", 13);
+		write(1, "start_program\n", 14);
+		parse_grid(clue, answer, argv[1]);
 	}
 	return (0);
 }
