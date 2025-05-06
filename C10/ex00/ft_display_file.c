0427689a7b2540689e203beb776d78284e553fb2
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-06 17:48:22 +0800

    finished ft_display_file with makefile

--- C10/ex00/ft_display_file.c
+++ C10/ex00/ft_display_file.c
@@ -6,8 +6,47 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:50:45 by kchiang           #+#    #+#             */
-/*   Updated: 2025/04/10 14:51:14 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/06 17:47:06 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//start writing dang it
+#include <fcntl.h>
+#include <unistd.h>
+
+void	ft_display_file(char *filename)
+{
+	int		file_descriptor;
+	ssize_t	file_bytes;
+	char	host;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		write(1, "Cannot read file.", 17);
+	else
+	{
+		file_bytes = read(file_descriptor, &host, 1);
+		while (file_bytes)
+		{
+			if (file_bytes == -1)
+			{
+				write(1, "Cannot read file.", 17);
+				break ;
+			}
+			write(1, &host, 1);
+			file_bytes = read(file_descriptor, &host, 1);
+		}
+		close(file_descriptor);
+	}
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		write(1, "File name missing.", 18);
+	else if (argc > 2)
+		write(1, "Too many arguments.", 19);
+	else
+		ft_display_file(argv[1]);
+	return (0);
+}
