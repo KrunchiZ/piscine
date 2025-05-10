0ea5c8a2e7bfae96e7e1b3d6af33e276c90755ac
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-11 04:14:45 +0800

    ft_tail wip

--- C10/ex02/ft_tail.c
+++ C10/ex02/ft_tail.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 00:42:25 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/10 18:12:50 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/11 04:14:16 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,52 @@
 #include <stdlib.h>
 #include <libgen.h>
 
-void	ft_tail(argc, argv, tail_bytes)
+void	ft_puterror(char *str);
+
+void	initialize_buffer(char *buffer, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+		buffer[i++] = 0;
+	return ;
+}
+
+void	exec_tail_stdin(char *buffer, int n_bytes)
+{
+	initialize_buffer(buffer, n_bytes);
+}
+
+int	exec_tail(char *filename, char *buffer, int n_bytes)
+{
+	initialize_buffer(buffer, n_bytes);
+	return (0);
+}
+
+int	ft_tail(int argc, char **argv, int n_bytes, int c_pos)
+{
+	int		i;
+	char	*buffer;
+
+	buffer = (char *)malloc(n_bytes);
+	if (buffer == NULL)
+	{
+		ft_puterror(basename(argv[0]));
+		ft_puterror(": malloc failed\n");
+		return (1);
+	}
+	if (argc == 3)
+		exec_tail_stdin(buffer, n_bytes);
+	else
+	{
+		i = 0;
+		while (++i < argc)
+		{
+			if ((i != c_pos && i != c_pos + 1)
+				&& (exec_tail(argv[i], buffer, n_bytes)))
+				return (1);
+		}
+	}
+	return (0);
+}
--- C10/ex02/main.c
+++ C10/ex02/main.c
@@ -6,14 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 00:42:25 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/11 03:06:29 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/11 04:06:33 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <libgen.h>
 
-void	ft_tail(int argc, char **argv, int n_bytes);
+void	ft_tail(int argc, char **argv, int n_bytes, int c_pos);
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -42,10 +42,10 @@ int	check_option(int argc, char **argv)
 	return (i);
 }
 
-void	ft_putstr(char *str)
+void	ft_puterror(char *str)
 {
 	while (*str)
-		write(1, str++, 1);
+		write(2, str++, 1);
 	return ;
 }
 
@@ -70,26 +70,24 @@ int	ft_atoi(char *str)
 int	main(int argc, char **argv)
 {
 	int		c_pos;
-	int		tail_bytes;
-	char	*program_name;
+	int		n_bytes;
 
-	program_name = basename(argv[0]);
 	c_pos = check_option(argc, argv);
 	if (c_pos == argc - 1)
 	{
-		ft_putstr(program_name);
-		ft_putstr(": option requires an argument -- 'c'\n");
+		ft_puterror(basename(argv[0]));
+		ft_puterror(": option requires an argument -- 'c'\n");
 		return (1);
 	}
-	tail_bytes = ft_atoi(argv[c_pos + 1]);
-	if (tail_bytes == -1)
+	n_bytes = ft_atoi(argv[c_pos + 1]);
+	if (n_bytes == -1)
 	{
-		ft_putstr(program_name);
-		ft_putstr(": invalid number of bytes: ‘");
-		ft_putstr(argv[c_pos + 1]);
-		ft_putstr("’\n");
+		ft_puterror(basename(argv[0]));
+		ft_puterror(": invalid number of bytes: ‘");
+		ft_puterror(argv[c_pos + 1]);
+		ft_puterror("’\n");
 		return (1);
 	}
-	ft_tail(argc, argv, tail_bytes);
+	ft_tail(argc, argv, n_bytes, c_pos);
 	return (0);
 }
