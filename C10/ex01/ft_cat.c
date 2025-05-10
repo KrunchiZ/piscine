8b5a3050e0cdf860fd657ce8e0ee4532f24a2dd9
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-05-11 03:40:29 +0800

    fixed error code write fd to 2 in ft_cat

--- C10/ex01/ft_cat.c
+++ C10/ex01/ft_cat.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:50:45 by kchiang           #+#    #+#             */
-/*   Updated: 2025/05/08 17:05:41 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/05/11 03:39:57 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ void	print_error(char *filename, char *program)
 	char	*error_code;
 
 	while (*program)
-		write(1, program++, 1);
-	write(1, ": ", 2);
+		write(2, program++, 1);
+	write(2, ": ", 2);
 	while (*filename)
-		write(1, filename++, 1);
-	write(1, ": ", 2);
+		write(2, filename++, 1);
+	write(2, ": ", 2);
 	error_code = strerror(errno);
 	while (*error_code)
-		write(1, error_code++, 1);
-	write(1, "\n", 1);
+		write(2, error_code++, 1);
+	write(2, "\n", 1);
 	return ;
 }
 
